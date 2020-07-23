186817f3b2af063c12878adba9447778e932f56a
Yoh Deadfall <yoh.deadfall@hotmail.com>
2020-07-23 20:31:10 +0300

    Added highlight text block

---
+++ src/Yoh.Windows.Controls/HighlightTextBlock.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace Yoh.Windows.Controls
+{
+    public class HighlightTextBlock : TextBlock
+    {
+        private sealed class TextPropertyMetadata : FrameworkPropertyMetadata
+        {
+            protected override void Merge(PropertyMetadata baseMetadata, DependencyProperty dp)
+            {
+                BaseTextChanged = baseMetadata.PropertyChangedCallback;
+
+                base.Merge(baseMetadata, dp);
+                PropertyChangedCallback = (d, e) => ((HighlightTextBlock)d).OnTextChanged(e);
+            }
+        }
+
+        private static Binding HighlightBinding;
+        private static PropertyChangedCallback BaseTextChanged;
+        private bool _textChanging;
+
+        static HighlightTextBlock() => TextProperty.OverrideMetadata(typeof(HighlightTextBlock), new TextPropertyMetadata());
+
+        public static readonly DependencyProperty HighlightProperty = DependencyProperty.RegisterAttached(
+            nameof(Highlight),
+            typeof(Brush),
+            typeof(HighlightTextBlock),
+            new FrameworkPropertyMetadata(
+                Brushes.Transparent,
+                FrameworkPropertyMetadataOptions.AffectsRender |
+                FrameworkPropertyMetadataOptions.SubPropertiesDoNotAffectRender |
+                FrameworkPropertyMetadataOptions.Inherits));
+
+        public Brush Highlight
+        {
+            get => (Brush)GetValue(HighlightProperty);
+            set => SetValue(HighlightProperty, value);
+        }
+
+        public static readonly DependencyProperty HighlightedTextProperty = DependencyProperty.Register(
+            nameof(HighlightedText),
+            typeof(string),
+            typeof(HighlightTextBlock),
+            new FrameworkPropertyMetadata(
+                string.Empty,
+                FrameworkPropertyMetadataOptions.AffectsMeasure |
+                FrameworkPropertyMetadataOptions.AffectsRender,
+                (d, e) => ((HighlightTextBlock)d).OnTextChanged(e),
+                (d, v) => v ?? string.Empty));
+
+        public string HighlightedText
+        {
+            get => (string)GetValue(HighlightedTextProperty);
+            set => SetValue(HighlightedTextProperty, value);
+        }
+
+        private void OnTextChanged(DependencyPropertyChangedEventArgs e)
+        {
+            if (_textChanging)
+                return;
+
+            try
+            {
+                _textChanging = true;
+
+                var text = Text;
+                var highlighted = HighlightedText;
+
+                if (highlighted.Length == 0)
+                {
+                    BaseTextChanged(this, e.Property == TextProperty
+                        ? e : new DependencyPropertyChangedEventArgs(TextProperty, text, text));
+                }
+                else
+                {
+                    Inlines.Clear();
+                    Inlines.AddRange(GetInlines());
+                }
+
+                IEnumerable<Run> GetInlines()
+                {
+                    for (int start, end = 0; end < text.Length;)
+                    {
+                        start = text.IndexOf(highlighted, end);
+
+                        if (start < 0)
+                        {
+                            yield return new Run(text.Substring(end));
+                            yield break;
+                        }
+
+                        if (start > end)
+                            yield return new Run(text.Substring(end, start - end));
+
+                        if (HighlightBinding is null)
+                            HighlightBinding = new Binding
+                            {
+                                Path = new PropertyPath(HighlightProperty),
+                                RelativeSource = RelativeSource.Self
+                            };
+
+                        var highlight = new Run();
+
+                        highlight.SetBinding(TextElement.BackgroundProperty, HighlightBinding);
+                        highlight.Text = text.Substring(start, highlighted.Length);
+
+                        yield return highlight;
+                        end = start + highlighted.Length;
+
+                    }
+                }
+            }
+            finally
+            {
+                _textChanging = false;
+            }
+        }
+    }
+}
